d8ac814e99ce1fa31f53d8aeee412ea1fec2f7f4
drjenn <djennings618@gmail.com>
2020-06-02 21:00:44 -0500

    updated the api

--- src/page_counter.js
+++ src/page_counter.js
@@ -3,7 +3,7 @@ const countEl = document.getElementById('count');
 updateVisitCount();
 
 function updateVisitCount() {
-    fetch('https://663p87dwcc.execute-api.us-east-2.amazonaws.com/prod/count')
+    fetch('https://bv1vhfqojc.execute-api.us-east-2.amazonaws.com/prod')
         .then(res => res.json())
         .then(res => {
         countEl.innerHTML = res.visits;
