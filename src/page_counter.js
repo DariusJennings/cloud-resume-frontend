22da05d042861710d6a558d7cdd0feca6d5e5a8c
drjenn <djennings618@gmail.com>
2020-06-20 23:16:57 -0500

    updated api

--- src/page_counter.js
+++ src/page_counter.js
@@ -3,7 +3,7 @@ const countEl = document.getElementById('count');
 updateVisitCount();
 
 function updateVisitCount() {
-    fetch('https://0aw077vxbk.execute-api.us-east-2.amazonaws.com/prod/count')
+    fetch('https://db0u1sqe9d.execute-api.us-east-2.amazonaws.com/prod/count')
         .then(res => res.json())
         .then(res => {
         countEl.innerHTML = res.visits;
